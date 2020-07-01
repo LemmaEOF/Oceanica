d75363996f4da051619fc8a84ba4ba524ec3636d
Meredith Espinosa <Boundarybreaker@users.noreply.github.com>
2020-06-30 23:30:59 -0700

    Episode 1: Setup (initial commit)

---
+++ src/main/java/space/bbkr/oceanica/Oceanica.java
@@ -0,0 +1,10 @@
+package space.bbkr.oceanica;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Oceanica implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Oceanica is initializing!");
+    }
+}
